f54fde5384a09813d9894338c210dec27a6c1793
Christian Bouwense <bouwensec@gmail.com>
2022-12-04 17:26:27 -0500

    lint and format

--- src/lib/utils/letterDataUtils.test.ts
+++ src/lib/utils/letterDataUtils.test.ts
@@ -5,95 +5,95 @@ vi.mock('svelte/store', () => ({ get: vi.fn() }));
 vi.mock('$lib/store', () => ({ gameProgress: 0 }));
 
 describe('createLetterData', () => {
-  describe('given an array of letters', () => {
-    it('creates a new array with each having a state included', () => {
-      const letterArray: string[] = ['a', 'b', 'c'];
+	describe('given an array of letters', () => {
+		it('creates a new array with each having a state included', () => {
+			const letterArray: string[] = ['a', 'b', 'c'];
 
-      expect(createLetterData(letterArray)).toEqual([
-        {
-          value: 'a',
-          state: 'inPlay'
-        },
-        {
-          value: 'b',
-          state: 'inPlay'
-        },
-        {
-          value: 'c',
-          state: 'inPlay'
-        }
-      ]);
-    });
-  });
+			expect(createLetterData(letterArray)).toEqual([
+				{
+					value: 'a',
+					state: 'inPlay'
+				},
+				{
+					value: 'b',
+					state: 'inPlay'
+				},
+				{
+					value: 'c',
+					state: 'inPlay'
+				}
+			]);
+		});
+	});
 
-  describe('given an empty array', () => {
-    it('should return an empty array', () => {
-      const letterArray: string[] = [];
-  
-      expect(createLetterData(letterArray)).toEqual([]);
-    });
-  });
+	describe('given an empty array', () => {
+		it('should return an empty array', () => {
+			const letterArray: string[] = [];
+
+			expect(createLetterData(letterArray)).toEqual([]);
+		});
+	});
 });
 
 describe('formWord', () => {
-  describe('given an array of LetterData', () => {
-    it('should return a string of a formed word.', () => {
-      const letterDataArray: LetterData[] = [
-        { value: 'x', state: 'used' },
-        { value: 'h', state: 'firstLetter' },
-        { value: 'o', state: 'inPlay' },
-        { value: 'w', state: 'inPlay' }
-      ];
+	describe('given an array of LetterData', () => {
+		it('should return a string of a formed word.', () => {
+			const letterDataArray: LetterData[] = [
+				{ value: 'x', state: 'used' },
+				{ value: 'h', state: 'firstLetter' },
+				{ value: 'o', state: 'inPlay' },
+				{ value: 'w', state: 'inPlay' }
+			];
 
-      expect(formWord(letterDataArray)).toEqual('how');
-    });
-  });
+			expect(formWord(letterDataArray)).toEqual('how');
+		});
+	});
 
-  describe('given an array of LetterData', () => {
-    it('should return a string of a formed word.', () => {
-      const letterDataArray: LetterData[] = [
-        { value: 'x', state: 'used' },
-        { value: 'h', state: 'firstLetter' },
-        { value: 'o', state: 'inPlay' },
-        { value: 'w', state: 'inPlay' }
-      ];
+	describe('given an array of LetterData', () => {
+		it('should return a string of a formed word.', () => {
+			const letterDataArray: LetterData[] = [
+				{ value: 'x', state: 'used' },
+				{ value: 'h', state: 'firstLetter' },
+				{ value: 'o', state: 'inPlay' },
+				{ value: 'w', state: 'inPlay' }
+			];
 
-      expect(formWord(letterDataArray)).toEqual('how');
-    });
-  });
+			expect(formWord(letterDataArray)).toEqual('how');
+		});
+	});
 });
 
 describe('makeWordUsed', () => {
-  it('should return a the letter array with the word specified used.', () => {
-    const letterDataArray: LetterData[] = [
-      { value: 'x', state: 'used' },
-      { value: 'h', state: 'firstLetter' },
-      { value: 'o', state: 'inPlay' },
-      { value: 'w', state: 'inPlay' }
-    ];
+	it('should return a the letter array with the word specified used.', () => {
+		const letterDataArray: LetterData[] = [
+			{ value: 'x', state: 'used' },
+			{ value: 'h', state: 'firstLetter' },
+			{ value: 'o', state: 'inPlay' },
+			{ value: 'w', state: 'inPlay' }
+		];
 
-    const expectedLetterArray: LetterData[] = [
-      { value: 'x', state: 'used' },
-      { value: 'h', state: 'used' },
-      { value: 'o', state: 'used' },
-      { value: 'w', state: 'firstLetter' }
-    ];
+		const expectedLetterArray: LetterData[] = [
+			{ value: 'x', state: 'used' },
+			{ value: 'h', state: 'used' },
+			{ value: 'o', state: 'used' },
+			{ value: 'w', state: 'firstLetter' }
+		];
 
-    expect(makeWordUsed(letterDataArray, 'how')).toEqual(expectedLetterArray);
-  });
+		expect(makeWordUsed(letterDataArray, 'how')).toEqual(expectedLetterArray);
+	});
 
-  describe('when the word does not match up with the letter array', () => {
-    it('should return a the letter array with the word specified used.', () => {
-      const letterDataArray: LetterData[] = [
-        { value: 'x', state: 'used' },
-        { value: 'h', state: 'firstLetter' },
-        { value: 'o', state: 'inPlay' },
-        { value: 'w', state: 'inPlay' }
-      ];
+	describe('when the word does not match up with the letter array', () => {
+		it('should return a the letter array with the word specified used.', () => {
+			const letterDataArray: LetterData[] = [
+				{ value: 'x', state: 'used' },
+				{ value: 'h', state: 'firstLetter' },
+				{ value: 'o', state: 'inPlay' },
+				{ value: 'w', state: 'inPlay' }
+			];
 
-      expect(() => {
-        makeWordUsed(letterDataArray, 'howl');
-      }).toThrow('Could not find matching word');
-    });
-  });
+			expect(() => {
+				makeWordUsed(letterDataArray, 'howl');
+			}).toThrow('Could not find matching word');
+		});
+	});
 });
--- src/lib/utils/wordUtils.test.ts
+++ src/lib/utils/wordUtils.test.ts
@@ -1,55 +1,55 @@
 import { checkIfValidWord, generateWord } from './wordUtils';
 
 describe('checkIfValidWord', () => {
-  describe('given a real word', () => {
-    it('returns true', () => {
-      expect(checkIfValidWord('Hello')).toBe(true);
-    });
-  });
+	describe('given a real word', () => {
+		it('returns true', () => {
+			expect(checkIfValidWord('Hello')).toBe(true);
+		});
+	});
 
-  describe('given a fake word', () => {
-    it('returns true', () => {
-      expect(checkIfValidWord('WelcomeToTheGrove')).toBe(false);
-    });
-  });
+	describe('given a fake word', () => {
+		it('returns true', () => {
+			expect(checkIfValidWord('WelcomeToTheGrove')).toBe(false);
+		});
+	});
 });
 
 describe('generateWord', () => {
-  it('returns a word having 5 letters', () => {
-    const word = generateWord(1);
+	it('returns a word having 5 letters', () => {
+		const word = generateWord(1);
 
-    expect(word.length).toBe(5);
-  });
+		expect(word.length).toBe(5);
+	});
 
-  describe('given a specific first letter', () => {
-    it('returns a word having 5 letters', () => {
-      const word = generateWord(1, 'h');
+	describe('given a specific first letter', () => {
+		it('returns a word having 5 letters', () => {
+			const word = generateWord(1, 'h');
 
-      expect(word.length).toBe(5);
-    });
+			expect(word.length).toBe(5);
+		});
 
-    it('returns a word with the same first letter as requested', () => {
-      const word = generateWord(1, 'h');
-      
-      expect(word[0]).toBe('h');
-    })
-  });
-  
-  describe('when invoked with the same seed', () => {
-    it('generates the same word each time', () => {
-      const word = generateWord(42);
-      const word2 = generateWord(42);
+		it('returns a word with the same first letter as requested', () => {
+			const word = generateWord(1, 'h');
 
-      expect(word).toBe(word2);
-    });
-  });
+			expect(word[0]).toBe('h');
+		});
+	});
 
-  describe('when invoked with different seeds', () => {
-    it('generates the same word each time', () => {
-      const word = generateWord(17);
-      const word2 = generateWord(38);
+	describe('when invoked with the same seed', () => {
+		it('generates the same word each time', () => {
+			const word = generateWord(42);
+			const word2 = generateWord(42);
 
-      expect(word).not.toBe(word2);
-    });
-  });
+			expect(word).toBe(word2);
+		});
+	});
+
+	describe('when invoked with different seeds', () => {
+		it('generates the same word each time', () => {
+			const word = generateWord(17);
+			const word2 = generateWord(38);
+
+			expect(word).not.toBe(word2);
+		});
+	});
 });
