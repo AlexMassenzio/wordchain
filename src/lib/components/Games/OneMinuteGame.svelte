d1af97f0a920177e5779023a3313714b7e51cb44
Alex Massenzio <alexmassenzio@gmail.com>
2023-01-27 17:58:33 -0500

    added handlekeydown

--- src/lib/components/Games/OneMinuteGame.svelte
+++ src/lib/components/Games/OneMinuteGame.svelte
@@ -99,7 +99,7 @@
 </script>
 
 <svelte:window on:keydown={handleKeydown} />
-<input id="mobileKeyboardInput" type="text" inputmode="text" />
+<input id="mobileKeyboardInput" type="text" inputmode="text" on:keydown={handleKeydown} />
 
 <!-- {#if !gameComplete} -->
 <Timer bind:elapsed={timer} isCountingDown={true} />
