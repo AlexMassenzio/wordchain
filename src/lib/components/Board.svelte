621af79fd10bc8b038190221a3d9dcea8465e5d4
Alex Massenzio <alexmassenzio@gmail.com>
2024-12-19 23:51:25 -0500

    fixed issue with chrome/mobile scrolling with the board. no more smooth scroll for now :(

--- src/lib/components/Board.svelte
+++ src/lib/components/Board.svelte
@@ -14,23 +14,24 @@
 		if (letters) scrollToBottom(container);
 	});
 
-	const scrollToBottom = async (node: HTMLElement) => {
-		node.scroll({ left: node.scrollWidth, behavior: 'smooth' });
+	const scrollToBottom = (node: HTMLElement) => {
+		node.scrollLeft = node.scrollWidth;
 	};
 </script>
 
 <div class="relative">
 	<!-- Blur effect for wrong guesses -->
 	<div
-		class="absolute inset-0 m-auto w-96 rounded-md bg-red-500 blur-md
-				{wrongGuess ? 'opacity-100' : 'opacity-0'}
-				transition-all"
+		class="absolute inset-0 m-auto w-96 rounded-md bg-red-500 blur-md transition-all"
+		class:opacity-100={wrongGuess}
+		class:opacity-0={!wrongGuess}
 	/>
 
 	<div
 		bind:this={container}
-		class="mv-2 relative m-auto flex min-h-[84px] w-96 overflow-x-hidden rounded-md border-2 bg-bg p-2
-		{wrongGuess && 'animate-shake border-red-400'} transition-all"
+		class="mv-2 relative m-auto flex min-h-[84px] w-96 touch-pan-x snap-x overflow-x-hidden overscroll-contain rounded-md border-2 bg-bg p-2 transition-all"
+		class:animate-shake={wrongGuess}
+		class:border-red-400={wrongGuess}
 	>
 		{#each letters as letter, index}
 			<Letter {letter} on:mousedown={() => moveLetter(isHand, index)} />
